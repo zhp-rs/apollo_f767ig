15d08edea27fdc08bc399eed7c426ed221e82fd7
Zhong Huiping <jxwazxzhp@126.com>
2020-10-16 16:12:37 +0800

    init.
    Signed-off-by: Zhong Huiping <jxwazxzhp@126.com>

---
+++ src/bin/led.rs
@@ -0,0 +1,37 @@
+//! "Blinky" using delays instead of a timer
+
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+extern crate panic_halt;
+
+use cortex_m_rt::entry;
+use stm32f7xx_hal::{delay::Delay, device, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let p = device::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    let gpiob = p.GPIOB.split();
+    let mut led = gpiob.pb0.into_push_pull_output();
+
+    // Constrain clocking registers
+    let rcc = p.RCC.constrain();
+
+    // Configure clock and freeze it
+    let clocks = rcc.cfgr.sysclk(216.mhz()).freeze();
+
+    // Get delay provider
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    loop {
+        led.set_high().expect("GPIO can never fail");
+        delay.delay_ms(500_u16);
+
+        led.set_low().expect("GPIO can never fail");
+        delay.delay_ms(500_u16);
+    }
+}
